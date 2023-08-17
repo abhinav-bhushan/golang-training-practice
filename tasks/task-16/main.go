a14414e061ea604ab6ff56e3a927a89d4243fc15
Abhinav <abbhushan@victoria.com>
2023-08-17 17:00:31 +0530

    add task-16

--- tasks/task-16/main.go
+++ tasks/task-16/main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"os"
+	"strconv"
+	"strings"
 )
 
+type Slice []string
+type Map map[string]int
+
+type alias interface {
+	ToString()
+}
+
 func main() {
-	data, ok := os.ReadFile("file.txt")
-
-	if ok == nil {
-		errors.New("File not found")
+	var s Slice = []string{"Hello", "World"}
+	var m Map = make(map[string]int)
+	m["bangalore-1"] = 560086
+	m["bangalore-2"] = 560096
+	var IAlias []alias = []alias{s, m}
+	for _, a := range IAlias {
+		a.ToString()
 	}
-	fmt.Println(string(data))
+}
 
+func (s Slice) ToString() {
+	output := ""
+	for _, v := range s {
+		output += v + ","
+	}
+	output = strings.TrimSuffix(output, ",")
+	fmt.Println(output)
+}
+
+func (s Map) ToString() {
+	output := ""
+	for k, v := range s {
+		output += "Key:" + k + "," + "Value:" + strconv.Itoa(v) + ";"
+	}
+	output = strings.TrimSuffix(output, ";")
+	fmt.Println(output)
 }
